95202c7f8a19c682bac2b99c20effb3c6dd8fb68
DieCM <didaccampos@gmail.com>
2018-12-20 16:33:41 +0100

    Creacion controllers usuari

---
+++ app/Http/Controllers/usuari/C04_modificarUsuariController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C04_modificarUsuariController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/usuari/C05_eliminarUsuariController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C05_eliminarUsuariController extends Controller
+{
+    //
+}
