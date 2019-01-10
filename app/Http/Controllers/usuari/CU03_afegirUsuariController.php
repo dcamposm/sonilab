1a882a374085cf6173cfada60d1ec8fd7ef4b4ba
DieCM <didaccampos@gmail.com>
2019-01-10 18:18:05 +0100

    Modificaciones controladores

--- app/Http/Controllers/usuari/CU03_afegirUsuariController.php
+++ app/Http/Controllers/usuari/CU03_afegirUsuariController.php
@@ -13,12 +13,25 @@ class CU03_afegirUsuariController extends Controller
     }
     
     public function create() {
-        if (!isset($_POST['alias'])){
-            return call('pages', 'error');
+        if (!isset($post['alias'])){
+            return view('pages.error');
+            //return call('pages', 'error');
         }
         
-        $user = Usuari::alta($_POST['alias'],$_POST['nom'],$_POST['cog1'],$_POST['cog2'],$_POST['email'],$_POST['tel'],$_POST['pass']);
+        $user = new Usuari;
+        $user->alias_usuari = $post['alias'];
+        $user->dni_usuari = $post['dni'];
+        $user->nom_usuari = $post['nom '];
+        $user->primer_cognom_usuari = $post['cog1'];
+        $user->segon_cognom_usuari = $post['cog2'];
+        $user->email_usuari = $post['email'];
+        $user->telefon_usuari = $post['tel'];
+        $user->contrasenya_usuari = $post['pass'];
+        $user->id_departament = $post['id_dep'];
+        $user->save();
         
-        return view('usuari.createUsuaris.php', array('user'=>$user));
+        //$user = Usuari::alta($_POST['alias'],$_POST['dni'],$_POST['nom'],$_POST['cog1'],$_POST['cog2'],$_POST['email'],$_POST['tel'],$_POST['pass'],$_POST['id_dep']);
+        
+        return view('usuari.createUsuaris');
     }
 }
--- app/Http/Controllers/usuari/CU04_modificarUsuariController.php
+++ app/Http/Controllers/usuari/CU04_modificarUsuariController.php
@@ -8,21 +8,34 @@ use App\Http\Controllers\Controller;
 class CU04_modificarUsuariController extends Controller
 {
     public function formUpdate() {
-        if (!isset($_GET['alias'])) {
-            return call('pages', 'error');
+        if (!isset($get['alias'])) {
+            return view('pages.error');
         }
-
-        $user = Usuari::buscar($_GET['alias']);
+        
+        $user = Usuari::where('alias', '=', $get['alias'])->get();
+        //$user = Usuari::buscar($_GET['alias']);
         
         return view('usuari.modificarUsuaris', array('user'=>$user));
     }
 
     public function update() {
-        if (!isset($_GET['alias'])){
-            return call('pages', 'error');
+        if (!isset($get['alias'])){
+            return view('pages.error');
         }
-
-        Usuari::modificar($_GET['alias'],$_POST['nom'],$_POST['cog1'],$_POST['cog2'],$_POST['email'],$_POST['tel'],$_POST['pass']);
+        
+        $user = new Usuari;
+        $user->alias_usuari = $get['alias'];;
+        $user->dni_usuari = $post['dni'];
+        $user->nom_usuari = $post['nom '];
+        $user->primer_cognom_usuari = $post['cog1'];
+        $user->segon_cognom_usuari = $post['cog2'];
+        $user->email_usuari = $post['email'];
+        $user->telefon_usuari = $post['tel'];
+        $user->contrasenya_usuari = $post['pass'];
+        $user->id_departament = $post['id_dep'];
+        $user->save();
+        
+        //Usuari::modificar($get['alias'],$post['nom'],$post['cog1'],$post['cog2'],$post['email'],$post['tel'],$post['pass']);
         
         return redirect()->action('usuari\CU02_usuariController@index');
     }
