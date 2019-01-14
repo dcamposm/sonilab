94d0fe42d28658d34c08a8e3059c988bd75a12e8
DieCM <didaccampos@gmail.com>
2019-01-14 21:11:43 +0100

    Solucion controladores modificar y eliminar usuario 2

--- app/Http/Controllers/usuari/CU05_eliminarUsuariController.php
+++ app/Http/Controllers/usuari/CU05_eliminarUsuariController.php
@@ -8,12 +8,12 @@ use App\Usuari;
 
 class CU05_eliminarUsuariController extends Controller
 {
-    public function delete() {
-        if (!isset($_GET['alias'])) {
+    public function delete($alias_usuari) {
+        /*if (!isset($_GET['alias'])) {
             return view('pages.error');
-        }
+        }*/
         
-        $user = Usuari::where('alias', $_GET['alias'])->delete();     
+        $user = Usuari::where('alias', $alias_usuari)->delete();     
         //Usuari::eliminar($_GET['alias']);
         
         return redirect()->action('usuari\CU02_usuariController@index');
