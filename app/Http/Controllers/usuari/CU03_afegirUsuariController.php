95ce2d340364d7ea23b8b75cee7b12dddebd8b2b
DieCM <didaccampos@gmail.com>
2019-01-14 20:04:52 +0100

    Solucion error view

--- app/Http/Controllers/usuari/CU03_afegirUsuariController.php
+++ app/Http/Controllers/usuari/CU03_afegirUsuariController.php
@@ -33,6 +33,6 @@ class CU03_afegirUsuariController extends Controller
         
         //$user = Usuari::alta($_POST['alias'],$_POST['dni'],$_POST['nom'],$_POST['cog1'],$_POST['cog2'],$_POST['email'],$_POST['tel'],$_POST['pass'],$_POST['id_dep']);
         
-        return view('usuari.crear');
+        return view('usuari.createUsuaris');
     }
 }
