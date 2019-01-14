a1c52f4c03224c1d909f59ea89234f495bf39029
DieCM <didaccampos@gmail.com>
2019-01-14 20:48:34 +0100

    Correccion cotrolador modificar usuari

--- app/Http/Controllers/usuari/CU04_modificarUsuariController.php
+++ app/Http/Controllers/usuari/CU04_modificarUsuariController.php
@@ -19,20 +19,20 @@ class CU04_modificarUsuariController extends Controller
     }
 
     public function update() {
-        if (!isset($get['alias'])){
+        if (!isset($_GET['alias'])){
             return view('pages.error');
         }
         
         $user = new Usuari;
-        $user->alias_usuari = $get['alias'];
-        $user->dni_usuari = $post['dni'];
-        $user->nom_usuari = $post['nom '];
-        $user->primer_cognom_usuari = $post['cog1'];
-        $user->segon_cognom_usuari = $post['cog2'];
-        $user->email_usuari = $post['email'];
-        $user->telefon_usuari = $post['tel'];
-        $user->contrasenya_usuari = $post['pass'];
-        $user->id_departament = $post['id_dep'];
+        $user->alias_usuari = $_GET['alias'];
+        $user->dni_usuari = $_POST['dni'];
+        $user->nom_usuari = $_POST['nom '];
+        $user->primer_cognom_usuari = $_POST['cog1'];
+        $user->segon_cognom_usuari = $_POST['cog2'];
+        $user->email_usuari = $_POST['email'];
+        $user->telefon_usuari = $_POST['tel'];
+        $user->contrasenya_usuari = $_POST['pass'];
+        $user->id_departament = $_POST['id_dep'];
         $user->save();
         
         //Usuari::modificar($get['alias'],$post['nom'],$post['cog1'],$post['cog2'],$post['email'],$post['tel'],$post['pass']);
