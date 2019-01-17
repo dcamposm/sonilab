95e3429f4eef8f854faf3b5bce352070419a69cd
dcampos <dcampos@A0001.SLAB.local>
2019-01-17 09:13:10 +0100

    Modifi

---
+++ resources/views/personal/createPersonal.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.master')
+@section('content')
+{{--borrar los br y hacer css--}}
+
+<div>
+
+    <div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        Afegir Personal</div>
+
+    <div>
+        <form method="POST" action="{{ url('/personal/crear/formulari') }}">
+            {{ csrf_field() }}
+
+            <table>
+
+                <tr>
+                    <td>DNI: </td>
+                    <td><input type="text" name="dni" id="dni"></td>
+                </tr>
+
+                <tr>
+                    <td>Nom: </td>
+                    <td><input type="text" name="nom" id="nom"></td>
+                </tr>
+                
+                <tr>
+                    <td>Primer Congom: </td>
+                    <td><input type="text" name="cog1" id="cog1"></td>
+                </tr>
+
+                <tr>
+                    <td>Segon Cognom: </td>
+                    <td><input type="text" name="cog2" id="cog2"></td>
+                </tr>
+
+                <tr>
+                    <td>Email: </td>
+                    <td><input type="text" name="email" id="email"></td>
+                </tr>
+
+                <tr>
+                    <td>Telèfon: </td>
+                    <td><input type="number" name="telefon" id="telefon"></td>
+                </tr>
+
+                <tr>
+                    <td>Direccio: </td>
+                    <td><input type="text" name="direccio" id="direccio"></td>
+                </tr>
+
+                <tr>
+                    <td>Data Naixment: </td>
+                    <td><input type="date" name="datanaix" id="datanaix"></td>
+                </tr>
+                
+                <tr>
+                    <td>Sexe: </td> 
+                    
+                    <td><select name="sexe" id="sexe">
+                            <option>Selecciona departament...</option>
+                            {{-- @while --}}
+                            <option value="home">Home</option>
+                            <option value="dona">Dona</option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Nacionalitat: </td>
+                    <td><input type="text" name="nacionalitat" id="nacionalitat"></td>
+                </tr>
+
+                <tr>
+                    <td>NSS: </td>
+                    <td><input type="text" name="nss" id="nss"></td>
+                </tr>
+
+                <tr>
+                    <td>IBAN: </td>
+                    <td><input type="text" name="iban" id="iban"></td>
+                </tr>
+                
+                <tr>
+                    <td>Rol: </td> 
+
+                    <td><select name="rol" id="rol">
+                            <option>Selecciona departament...</option>
+                            {{-- @while --}}
+                            <option value="actor">Actor</option>
+                            <option value="director">Director de doblatge</option>
+                            <option value="tecnic_sala">Tècnic de sala</option>
+                            <option value="traductor">Traductor/Ajustador</option>
+                        </select></td>
+                </tr>
+
+            </table>
+
+            <button type="sumbit">Afegir personal</button>
+        </form>
+    </div>
+</div>
+
+@stop
---
+++ resources/views/personal/modificarPersonalDirector.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.master')
+@section('content')
+
+<div>
+    <div>Modificar Director</div>
+    <div>
+        <form method="POST" action="{{ url('/personal/modificar/formulari')}}/{{ $per->rol }}/{{ $per->dni_director }}">
+            
+            {{ csrf_field() }}
+            
+            <table>
+            
+                <tr>
+                    <td>Nom: </td>
+                    <td><input type="text" name="nom" id="nom" value="{{$per->nom_director}}"></td>
+                </tr>
+                
+                <tr>
+                    <td>Primer Congom: </td>
+                    <td><input type="text" name="cog1" id="cog1" value="{{$per->primer_cognom_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>Segon Cognom: </td>
+                    <td><input type="text" name="cog2" id="cog2" value="{{$per->segon_cognom_director}}" ></td>
+                </tr>
+
+                <tr>
+                    <td>Email: </td>
+                    <td><input type="text" name="email" id="email" value="{{$per->email_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>Telèfon: </td>
+                    <td><input type="number" name="telefon" id="telefon" value="{{$per->telefon_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>Direccio: </td>
+                    <td><input type="text" name="direccio" id="direccio" value="{{$per->direccio_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>Data Naixment: </td>
+                    <td><input type="date" name="datanaix" id="datanaix" value="{{$per->data_naixement_director}}"></td>
+                </tr>
+                
+                <tr>
+                    <td>Sexe: </td> 
+                    
+                    <td><select name="sexe" id="sexe">
+                            <option>{{$per->sexe_director}}</option>
+                            {{-- @while --}}
+                            <option value="home">Home</option>
+                            <option value="dona">Dona</option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Nacionalitat: </td>
+                    <td><input type="text" name="nacionalitat" id="nacionalitat" value="{{$per->nacionalitat_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>NSS: </td>
+                    <td><input type="text" name="nss" id="nss" value="{{$per->nss_director}}"></td>
+                </tr>
+
+                <tr>
+                    <td>IBAN: </td>
+                    <td><input type="text" name="iban" id="iban" value="{{$per->iban_director}}"></td>
+                </tr>
+                
+    
+            </table>
+            
+            <button type="sumbit">Modificar director</button>
+        </form>
+    </div>
+</div>
+
+@stop
