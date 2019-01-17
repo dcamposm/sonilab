92bc9553626049a6914053e0aedbee9ea1ec723d
Tsuki <Tsuki@DESKTOP-RU0UJKI>
2019-01-17 12:56:10 +0100

    view: navbar.blade -> class added

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,18 @@
 <header>
-                    <div class="header"> 
-                        <img src="http://localhost/sonilab/assets/images/sonilab_logo.png" alt="Sonilab Studios" class="logo">
-                        <div class="menu">
-                            <div class="nav">
-                                <a href='#'>Projecte</a>
-                                <a href='{{url('personal/index')}}'>Personal</a>
-                                <a href='#'>Calendari</a>
-                                <a href='#'>Registres</a>
-                                <a href='{{url('usuari/index')}}'>Usuaris</a>
-                                <a href='{{}}'>Perfil</a>
-                            </div>
-                            <a href='#'>
-                                <img src="http://localhost/sonilab/assets/images/logout.png" alt="Sonilab Studios" class="logout">
-                            </a>
-                        </div>
-                    </div>
-                </header>
+	<div class="header"> 
+		<img src="{!! asset('imgs/sonilab_logo.png') !!}" alt="Sonilab Studios" class="logo">
+		<div class="menu">
+			<div class="nav">
+				<a href='#'>Projecte</a>
+				<a href='{{url('personal/index')}}'>Personal</a>
+				<a href='#'>Calendari</a>
+				<a href='#'>Registres</a>
+				<a href='{{url('usuari/index')}}'>Usuaris</a>
+				<a href='{{}}'>Perfil</a>
+			</div>
+			<a href='#'>
+				<img src="{!! asset('imgs/logout.png') !!}" alt="Sonilab Studios" class="logout">
+			</a>
+		</div>
+	</div>
+</header>
