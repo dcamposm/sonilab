f462433278588c3378de8e7ddf9d6dc620dfd56f
DieCM <didaccampos@gmail.com>
2019-01-17 20:50:56 +0100

    Modificao

--- app/Http/Controllers/usuari/CU02_usuariController.php
+++ app/Http/Controllers/usuari/CU02_usuariController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\usuari;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Usuari;
 
 class CU02_usuariController extends Controller
 {
     public function index(){
         
-        $users = \App\Usuari::All();
+        $users = Usuari::All();
         //$users = Usuari::mostrarTots();
         return view('usuari.index', array('users'=>$users));
     }
